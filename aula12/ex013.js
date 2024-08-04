0e8b2d160b90f82563a34e4a4327440aa5ffdc3f
PH-All1ce <pedroshow789@gmail.com>
2024-08-04 00:57:41 -0300

    Avanços em condicionais e começo dos exercícios

--- aula12/ex013.js
+++ aula12/ex013.js
@@ -10,3 +10,30 @@ var diasem = agora.getDay()
     6 - Sábado
 */
 console.log(diasem)
+
+switch(diasem) {
+    case 0:
+        console.log('Domigo')
+        break
+    case 1:
+        console.log('Segunda')
+        break
+    case 2:
+        console.log('Terça')
+        break
+    case 3:
+        console.log('Quarta')
+        break
+    case 4:
+        console.log('Quinta')
+        break
+    case 5:
+        console.log('Sexta')
+        break
+    case 6:
+        console.log('Sábado')
+        break
+    default:
+        console.log('Error')
+        break
+}

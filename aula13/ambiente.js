31205caa73fa803d7a2a339abfe25c3018e740f1
PH-All1ce <pedroshow789@gmail.com>
2024-08-18 00:02:14 -0300

    Início das estruturas de repetição.

---
+++ aula13/ambiente.js
@@ -0,0 +1,11 @@
+var c = 1
+var d = 1
+while (c <= 6) {
+    console.log(`passo ${c}`)
+    c++
+}
+
+do {
+    console.log(`passo ${d}`)
+    d++
+} while (d <= 6)
--- exercicios01/script.js
+++ exercicios01/script.js
@@ -10,7 +10,7 @@ function verificar() {
         var fsex = window.document.getElementsByName('radsex')
         var idade = ano - Number(fano.value)
         var genero = ''
-        var img = window.document.createElement('img')
+        var img = document.createElement('img')
         img.setAttribute('id', 'foto')
 
         if (fsex[0].checked) {
@@ -20,7 +20,7 @@ function verificar() {
             } else if (idade > 18 && idade <= 50) {
                 img.setAttribute('src', 'foto.felipeneo.jpg')
             } else {
-                img.setAttribute('src', 'foto.bolsonaro.jpg')
+                img.setAttribute('src', 'foto.gusta2.jpg')
             }
         } else if (fsex[1].checked) {
             genero = 'Mulher'

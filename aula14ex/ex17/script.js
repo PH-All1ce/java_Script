a052a73194c7295c9842a4eadf6c7b70b12dc320
PH-All1ce <pedroshow789@gmail.com>
2024-08-25 11:44:50 -0300

    Exercícios de laços feitos.

--- aula14ex/ex17/script.js
+++ aula14ex/ex17/script.js
@@ -3,8 +3,17 @@ function Tabuada() {
     var resposta = window.document.getElementById('res')
     var num = Number(numero.value)
     var c = 1
-    for (c = 1; c <= 10; c++) {
-        var mult = num * c
-        resposta.innerHTML += `${mult}`
+    if (num == ''){
+        alert('por favor digite um número para que possa ser realizada a sua tabuada!')
+    } else {
+        for (c = 1; c <= 10; c++) {
+            var mult = num * c
+            resposta.innerHTML += `${c} x ${num} = ${mult} <br>`
+        }
     }
+}
+
+function Zerar() {
+    var resposta =  window.document.getElementById('res')
+    resposta.innerHTML = 'Valor zerado. <br>'
 }
